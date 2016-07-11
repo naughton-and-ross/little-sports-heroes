c5aab571c4c4170c0c84b4a6ff0f1a7c22e19514
William Naughton-Gravette <william@willng.me>
2016-07-11 15:34:56 +1000

    async font

--- assets/php/head.php
+++ assets/php/head.php
@@ -8,5 +8,13 @@
 <!--[if gt IE 8]><!-->
     <link rel="stylesheet" href="assets/css/pure/grids-responsive-min.css">
 <!--<![endif]-->
-<script src="https://use.typekit.net/zgo3nte.js"></script>
-<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<script>
+  (function(d) {
+    var config = {
+      kitId: 'zgo3nte',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
