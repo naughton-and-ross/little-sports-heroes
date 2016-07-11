24d252bbda16f65c36de7ab0511dd33a62197370
William Naughton-Gravette <william@willng.me>
2016-07-11 15:18:09 +1000

    init

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('assets/php/head.php'); ?>
+        <title>Little Sports Heroes</title>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div class="intro">
+                <div class="pure-g">
+                    <div class="pure-u-2-24 spacer">
+
+                    </div>
+                    <div class="pure-u-lg-3-24 pure-u-5-24 logo">
+                        <img class="pure-img" src="/assets/img/logo/trans.png" />
+                    </div>
+                    <div class="pure-u-5-24 spacer">
+
+                    </div>
+                    <div class="pure-u-12-24 menu">
+
+                    </div>
+                </div>
+                <div class="pure-g">
+                    <div class="pure-u-2-24 spacer">
+
+                    </div>
+                    <div class="pure-u-1-2">
+                        <div class="l-box">
+                            <h1>Your Kids Can Be <span>Little Sports Heroes!</span></h1>
+                            <p>
+                                Little Sports Heroes wants to [mission]
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <img class="pure-img" src="/assets/img/bg/kids.png" />
+            </div>
+        </div>
+    </body>
+</html>
