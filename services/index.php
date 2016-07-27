c0a5d00b8f901e9957791f7121338b8d32c97aca
William Naughton-Gravette <william@willng.me>
2016-07-27 10:58:48 +1000

    work mostly on What We Do page

--- services/index.php
+++ services/index.php
@@ -8,34 +8,68 @@
     <body>
         <div id="container">
             <?php include('../assets/php/pages_menu.php'); ?>
-            <div class="pure-u-1 banner">
-                <img class="pure-img" src="http://placehold.it/1500x500">
-            </div>
-            <div class="pure-u-1 page_intro" style="background-color: #e74c3c">
-                <div class="l-box">
-                    <h1>
-                        What We Do
-                    </h1>
+            <div class="pure-g page_intro banner wwd">
+                <div class="pure-u-1">
+                    <div class="l-box">
+                        <h1>
+                            What We Do
+                        </h1>
+                    </div>
                 </div>
-            </div>
-            <div class="pure-g body" style="background-color: #c0392b">
                 <div class="pure-u-1 pure-u-md-14-24">
                     <div class="l-box">
-                        <h2>A few words about what <strong>Little Sports Heroes</strong> can offer anybody looking to sign up for the service or look to have their child signed up into and existing program.</h2>
+                        <h2>At <strong>Litle Sports Heroes</strong>, our vision is to create a community of actively healthy children. As a company, we understand that there are enormous benefits to participating in sport and maintaining an active lifestyle.</h2>
                     </div>
                 </div>
             </div>
             <div class="pure-g">
-                <div class="pure-u-1 pure-u-md-14-24">
+                <div class="pure-u-1 pure-u-md-1-2 function_item odd">
                     <div class="l-box">
-                        <h2>Something we do</h2>
-                        <p>
-                            A little about this thing we do
-                        </p>
+                        <h2>Development-focused program</h2>
                     </div>
                 </div>
-                <div class="pure-u-1 pure-u-md-10-24 circle-imge">
-                    <img class="pure-img" src="http://placehold.it/400x300">
+                <div class="pure-u-1 pure-u-md-1-2 function_item even">
+                    <div class="l-box">
+                        <h2>Structured coaching</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-2 function_item odd">
+                    <div class="l-box">
+                        <h2>Teaches correct technqiue and terminology</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-2 function_item even">
+                    <div class="l-box">
+                        <h2>Follows the Early Learning Years Framework</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-2 function_item odd">
+                    <div class="l-box">
+                        <h2>Smaller groups for more personal coaching</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-2 function_item even">
+                    <div class="l-box">
+                        <h2>Unerstanding and listening to instructions</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-2 function_item odd">
+                    <div class="l-box">
+                        <h2>Teaches peer-encouragement</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-2 function_item even">
+                    <div class="l-box">
+                        <h2>Multiple sports over the year</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-2 function_item odd">
+                    <div class="l-box">
+                        <h2>Restoring the enjoyment of keeping active</h2>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-2">
+                    
                 </div>
             </div>
             <?php include('../assets/php/pages_footer.php'); ?>
