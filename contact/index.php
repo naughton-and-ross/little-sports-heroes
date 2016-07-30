bb2bd8cc3f1d60158a856b01cf926d55d386283d
William Naughton-Gravette <william@willng.me>
2016-07-31 01:03:20 +1000

    update copy

--- contact/index.php
+++ contact/index.php
@@ -24,12 +24,6 @@
                 </div>
             </div>
             <div class="pure-g body" style="background-color: #27ae60">
-                <div class="pure-u-1 pure-u-md-14-24">
-                    <div class="l-box">
-                        <h2>Want <strong>Little Sports Heroes</strong> in your school or centre? It couldn't be easier to grt started with us.</h2>
-                        <h2>Fill is the form and we'll get back to you as soon as possible!</h2>
-                    </div>
-                </div>
                 <div class="pure-u-1 pure-u-md-10-24">
                     <div class="l-box">
                         <form class="pure-form">
@@ -43,6 +37,12 @@
                             </fieldset>
                             <button type="submit" class="pure-button pure-input-1 pure-button-secondary">Send!</button>
                         </form>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-14-24">
+                    <div class="l-box">
+                        <h2><strong>Little Sports Heroes</strong> services the wider Melbourne area and can work in all kinds of centres including daycares, kindergartens, schools and other sports centres.</h2>
+                        <h2>If you're not sure if LSH can come to you, don't hesitate to get in touch to find out.</h2>
                     </div>
                 </div>
             </div>
