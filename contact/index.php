0c8700f3084f73d69d8a04f0cb3712298791e7d1
William‌‌ Naughton-Gravette <william@willng.me>
2016-08-15 21:32:59 +1000

    Spellcheck

--- contact/index.php
+++ contact/index.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="pure-u-1 pure-u-md-14-24 darken">
                     <div class="l-box">
-                        <h2>Want <strong>Little Sports Heroes</strong> in your school or centre? It couldn't be easier to grt started with us. Fill is the form and we'll get back to you as soon as possible!</h2>
+                        <h2>Want <strong>Little Sports Heroes</strong> in your school or centre? It couldn't be easier to get started with us. Fill is the form and we'll get back to you as soon as possible!</h2>
                     </div>
                 </div>
             </div>
