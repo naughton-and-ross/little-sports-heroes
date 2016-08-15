b4590dd639bbc5ec3219f4dcc0456355c5cbbd9e
William Naughton-Gravette <william@willng.me>
2016-08-16 00:33:26 +1000

    add slider and fb feed

--- contact/index.php
+++ contact/index.php
@@ -3,9 +3,19 @@
     <head>
         <?php include('../assets/php/head.php'); ?>
         <link rel="stylesheet" href="/assets/css/pages.css" />
+        <link rel="stylesheet" href="/assets/css/unslider.css">
+        <link rel="stylesheet" href="/assets/css/unslider-dots.css">
         <title>Litle Sports Heroes</title>
     </head>
     <body>
+        <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=666382366847597";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
         <div id="container">
             <?php include('../assets/php/pages_menu.php'); ?>
             <div class="pure-g page_intro banner contact">
@@ -51,9 +61,37 @@
                         <h1>Little Sports Heroes On Social Media</h1>
                     </div>
                 </div>
+                <div class="pure-u-1 pure-u-md-8-24">
+                    <div class="l-box">
+                        <div class="fb-page" data-href="https://www.facebook.com/LittleSportsHeroes/?fref=ts" data-tabs="timeline" data-width="800" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/LittleSportsHeroes/?fref=ts" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/LittleSportsHeroes/?fref=ts">Little Sports Heroes</a></blockquote></div>
+                    </div>
+                </div>
+                <div class="pure-u-1 pure-u-md-16-24">
+                    <div class="l-box">
+                        <div class="my-slider">
+                        	<ul>
+                        		<li><img class="pure-img" src="/assets/img/candid/IMG_0006.jpg" /></li>
+                        		<li><img class="pure-img" src="/assets/img/candid/IMG_0011.jpg" /></li>
+                        		<li><img class="pure-img" src="/assets/img/candid/IMG_0013.jpg" /></li>
+                                <li><img class="pure-img" src="/assets/img/candid/IMG_0016.jpg" /></li>
+                                <li><img class="pure-img" src="/assets/img/candid/IMG_0002.jpg" /></li>
+                        	</ul>
+                        </div>
+                    </div>
+
+                </div>
             </div>
             <?php include('../assets/php/pages_footer.php'); ?>
         </div>
-
+        <script src="/assets/js/unslider-min.js"></script>
+        <script>
+		jQuery(document).ready(function($) {
+			$('.my-slider').unslider({
+                nav: false,
+                autoplay: true,
+                arrows: false
+            });
+		});
+	</script>
     </body>
 </html>
